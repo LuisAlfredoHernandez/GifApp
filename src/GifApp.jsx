9e178e5381d28564c466c64051d76cd01da6ceae
Luis Hernandez <luiszah36@gmail.com>
2023-06-23 16:22:56 -0400

    adding css file and main component creation

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
+
 const GifApp = () => {
+
+    const [categories, setCategories] = useState(['onePunch', 'dragonBall']);
+    console.log(categories)
     return (
         <>
+            <h1>GifApp</h1>
+            <ol>
+                {categories.map(cat=> <li key={cat}>{cat}</li>)}
+            </ol>
         </>
     );
 }
